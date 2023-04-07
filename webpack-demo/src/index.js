1604dd3f88784681edd995adabb235bbd6932fc1
OscarV <oovillag@gmail.com>
2023-04-07 11:41:53 -0500

    Add event listener for clear button

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -48,3 +48,9 @@ activityField.forEach((field, i) => {
   });
 });
 
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', () => {
+  taskManager.taskList = taskManager.taskList.filter((task) => !task.completed);
+  taskManager.saveTasks();
+  window.location.reload();
+})
